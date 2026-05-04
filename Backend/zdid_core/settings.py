42f2e71c770583e5fb95f2e099d8245472b89055
Hamusonde <noisemakers11@gmail.com>
2026-05-04 21:40:58 +0200

    feat: initialize project settings with custom user model, signing keys, and database configuration

--- Backend/zdid_core/settings.py
+++ Backend/zdid_core/settings.py
@@ -162,8 +162,9 @@ STATICFILES_DIRS = [
 # Email settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.googlemail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 EMAIL_TIMEOUT = 10
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
